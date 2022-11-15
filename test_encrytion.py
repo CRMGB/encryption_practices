153d88857412b9a51afd205b3399e419ac85197c
Cristian Martinez <cristianbisby@gmail.com>
2022-11-15 09:43:13 +0000

    Added docstring to the test

--- test_encrytion.py
+++ test_encrytion.py
@@ -1,18 +1,21 @@
 import io
 import os
 import unittest
-from encryption import encrypt_file, decrypt_file
+from . import encrypt_file, decrypt_file
 
 
 class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
         # <-- absolute dir the script is in
-        self.script_dir = os.path.dirname(__file__)
+        self.script_dir = os.path.dirname(__file__)        
         self.file_to_encrypt = io.BytesIO()
 
 class EncryptTestCase(BaseTestCase):
     def test_ensure_the_file_to_upload_is_encrypted_and_able_to_decrypt(self):
+        """Expect the file file uploaded is encrypted.
+        Also expect the file is decrypted when we call the decrypt_file method.
+        """
         # Read example file
         rel_path = "test_files/test_file_1.txt"
         abs_file_path = os.path.join(self.script_dir, rel_path)
